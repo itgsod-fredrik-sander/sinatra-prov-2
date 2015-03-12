b042b8e70c3754a9b5e5cc72c3198df332713040
Freddan962 <fredrik.sander@Fredriks-MacBook-Pro.local>
2015-03-12 11:54:05 +0100

    Commenting

--- app.rb
+++ app.rb
@@ -8,14 +8,17 @@ class App < Sinatra::Base
     slim :index
   end
 
+  # Returns all the cinemas which shows the movie by movie id
   get '/movie/:id/biographs' do |id|
     Movie.get(id).showings.cinemas.to_json
   end
 
+  # Returns all showings that show the movie by movie id
   get '/movie/:movie_id/biographs/:cinema_id/showings' do |movie_id, cinema_id|
     Movie.get(movie_id).showings.all(:cinema_id => cinema_id).to_json
   end
 
+  # Returns all movies the cinema shows by cinema id
   get '/cinema/:id/movies' do |id|
     showings = Cinema.get(1).showings
     movies = []
@@ -24,6 +27,7 @@ class App < Sinatra::Base
     movies.uniq {|movie| movie.id}.to_json
   end
 
+  # Returns all showings in the one cinema and by one movie id
   get '/cinema/:cinema_id/movies/:movie_id/showings' do |cinema_id, movie_id|
     Cinema.get(cinema_id).showings.all(:movie_id => movie_id).to_json
   end
