ad2e1c079c8e11bb7248b598f2930a7281f55ad6
Freddan962 <fredrik.sander@Fredriks-MacBook-Pro.local>
2015-03-12 10:29:06 +0100

    Renamed variable

--- app.rb
+++ app.rb
@@ -11,7 +11,7 @@ class App < Sinatra::Base
     Movie.get(id).showings.cinemas.to_json
   end
 
-  get '/movie/:movie_id/biographs/:cinema_id/showings' do |movie_id, biograph_id|
+  get '/movie/:movie_id/biographs/:cinema_id/showings' do |movie_id, cinema_id|
     Movie.get(movie_id).showings.all(:cinema_id => cinema_id)
   end
 end
