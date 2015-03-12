ab736cd387436af34993ea87185495fb1a78a3fe
Freddan962 <fredrik.sander@Fredriks-MacBook-Pro.local>
2015-03-12 09:43:55 +0100

    Converts movie_id to string

--- public/js/functionality.js
+++ public/js/functionality.js
@@ -1,5 +1,5 @@
 $('#movie').change(function() {
-  var movie_id = $(this).val(); 
+  var movie_id = $(this).val().toString(); 
 
   $.get('/movies/', function (res) {
     var data = $.parseJSON(res);
