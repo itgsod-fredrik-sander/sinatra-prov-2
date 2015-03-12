ca0bb54d09d5418b6d6dfc7a51b7a0c3f59739e1
Freddan962 <fredrik.sander@Fredriks-MacBook-Pro.local>
2015-03-12 10:45:59 +0100

    Get path that returns unique movies

--- app.rb
+++ app.rb
@@ -15,4 +15,10 @@ class App < Sinatra::Base
   get '/movie/:movie_id/biographs/:cinema_id/showings' do |movie_id, cinema_id|
     Movie.get(movie_id).showings.all(:cinema_id => cinema_id).to_json
   end
+
+  get '/cinema/:id/movies' do |id|
+    showings = Cinema.get(1).showings
+    movies = showings.map {|showing| Movie.get(showing.movie_id)}
+    movies.unique {|movie| movie.id}
+  end
 end
