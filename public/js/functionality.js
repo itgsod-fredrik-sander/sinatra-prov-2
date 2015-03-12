0ebc1a4ca2a040ad8aa2065d0d701375be0ee1f1
Freddan962 <fredrik.sander@Fredriks-MacBook-Pro.local>
2015-03-12 09:44:41 +0100

    Joins movie_id with the get request's route

--- public/js/functionality.js
+++ public/js/functionality.js
@@ -1,7 +1,7 @@
 $('#movie').change(function() {
   var movie_id = $(this).val().toString(); 
 
-  $.get('/movies/', function (res) {
+  $.get('/movies/' + movie_id, function (res) {
     var data = $.parseJSON(res);
     loadPath(data, 0);
   });
