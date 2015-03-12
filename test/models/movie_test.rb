8a39bc2339deaededed582613d815f8f9be01744
Freddan962 <fredrik.sander@Fredriks-MacBook-Pro.local>
2015-03-12 11:31:10 +0100

    Proper it should not work

--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -2,13 +2,19 @@ require_relative 'models_helper'
 
 describe Movie do 
   before do
-    @movie = Movie.first(name: 'The Duplo Movie')
+    DataMapper.auto_migrate!
+    
+    @movie = Movie.create(name: 'The Duplo Movie')
+    @cinema = Cinema.create(name: 'Bioslottet')
+    @room = Room.create(name: 'Sal 1', cinema_id: @cinema.id, number_of_seats: 240)
+    @showing = Showing.create(movie_id: @movie.id, cinema: @cinema, room: @room, start_time: DateTime.strptime('2015-03-13 21:15', '%Y-%m-%d %H:%M') )
   end
 
   describe 'associations' do 
 
     it 'should not work' do 
-      expect (@movie.showings.first.start_time).to match '2015-03-13T21:15:00+00:00 + 50'
+      expect( @movie.showings.first.room.name ).to match 'test'
+      #expect (@movie.showings.first.start_time).to_s.to_match '2015-03-13T21:15:00+00:00'
     end
 
   end
