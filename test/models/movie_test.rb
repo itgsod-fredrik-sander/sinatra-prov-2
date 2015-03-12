8156565ded82388e2d62357e7b1b9c5e2828baed
Freddan962 <fredrik.sander@Fredriks-MacBook-Pro.local>
2015-03-12 11:43:54 +0100

    Association tests

--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -10,13 +10,48 @@ describe Movie do
     @showing = Showing.create(movie_id: @movie.id, cinema: @cinema, room: @room, start_time: DateTime.strptime('2015-03-13 21:15', '%Y-%m-%d %H:%M') )
   end
 
-  describe 'associations' do 
+  describe 'movie associations' do 
 
-    it 'should not work' do 
-      expect( @movie.showings.first.room.name ).to match 'test'
-      #expect (@movie.showings.first.start_time).to_s.to_match '2015-03-13T21:15:00+00:00'
+    it 'should return the correct showing start time' do 
+      expect( @movie.showings.first.start_time.to_s).to match '2015-03-13T21:15:00+00:00'
     end
 
   end
 
+  describe 'cinema associations' do 
+
+    it 'should return the correct room name' do 
+      expect( @cinema.rooms.first.name ).to match 'Sal 1'
+    end
+
+    it 'should return the correct showing start time' do 
+      expect( @cinema.showings.first.start_time.to_s).to match '2015-03-13T21:15:00+00:00'
+    end
+  end
+
+  describe 'room associations' do 
+    it 'should return the correct cinema name' do 
+      expect( @room.cinema.name).to match 'Bioslottet'
+    end
+
+    it 'should return the correct showing start time' do 
+      expect( @room.showings.first.start_time.to_s).to match '2015-03-13T21:15:00+00:00'
+    end
+  end
+
+  describe 'showing associations' do 
+    it 'should return the correct movie name' do 
+      expect( @showing.movie.name ).to match 'The Duplo Movie'
+    end
+
+    it 'should return the correct room name' do 
+      expect( @showing.room.name ).to match 'Sal 1'
+    end
+
+    it 'should return the correct cinema name' do 
+      expect( @showing.cinema.name ).to match 'Bioslottet'
+    end
+  end
+
+  describe ''
 end
